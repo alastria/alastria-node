de18468dcea14f0f4d917f20913996a8d9d7ddce
Marcos Serradilla Diez <marcos@sanlab.io>
2017-11-16 15:22:11 +0100

    Cambio del network id para evitar colisiones con la test-net de la versión Quorum 1.1.0 y Constellation 0.1.0

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 set -u
 set -e
 
-NETID=96323
+NETID=963232369
 GLOBAL_ARGS="--networkid $NETID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
 
 _TIME=$(date +%Y%m%d%H%M%S)
