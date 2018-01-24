41db6721eceb52154a36f06f9a2d48b0d84bc44e
arocha <alfon.rocha@gmail.com>
2018-01-24 13:10:16 +0100

    monitor script fixed

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,26 +4,21 @@ set -e
 
 _TIME=$(date +%Y%m%d%H%M%S)
 
-script ~/alastria/logs/monitor__"${_TIME}".log
+echo `lsof -i | grep *:21000 | awk '{print $8 $9 $10}'` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `lsof -i | grep *:22000 | awk '{print $8 $9 $10}'` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `lsof -i | grep *:9000 | awk '{print $8 $9 $10}'` >> ~/alastria/logs/monitor__"${_TIME}".log
 
-lsof -i | grep *:21000 | awk '{print $8 $9 $10}'
-lsof -i | grep *:22000 | awk '{print $8 $9 $10}'
-lsof -i | grep *:9000 | awk '{print $8 $9 $10}'
-
-geth -exec 'admin.nodeInfo' attach ~/alastria/data/geth.ipc
-geth -exec 'admin.peers.length' attach ~/alastria/data/geth.ipc
-geth -exec 'admin.peers' attach ~/alastria/data/geth.ipc
-geth -exec 'eth.blockNumber' attach ~/alastria/data/geth.ipc
-geth -exec 'eth.mining' attach ~/alastria/data/geth.ipc
-geth -exec 'eth.syncing' attach ~/alastria/data/geth.ipc
-geth -exec 'eth.pendingTransactions' attach ~/alastria/data/geth.ipc
-geth -exec 'istanbul.candidates' attach ~/alastria/data/geth.ipc
-geth -exec 'istanbul.getValidators()' attach ~/alastria/data/geth.ipc
-geth -exec 'net.peerCount' attach ~/alastria/data/geth.ipc
-geth -exec 'net.version' attach ~/alastria/data/geth.ipc
-geth -exec 'txpool.content' attach ~/alastria/data/geth.ipc
-
-exit
+echo `geth -exec 'admin.nodeInfo' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'admin.peers' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'eth.blockNumber' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'eth.mining' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'eth.syncing' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'eth.pendingTransactions' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'istanbul.candidates' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'istanbul.getValidators()' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'net.peerCount' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'net.version' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
+echo `geth -exec 'txpool.content' attach ~/alastria/data/geth.ipc` >> ~/alastria/logs/monitor__"${_TIME}".log
 
 set +u
 set +e
