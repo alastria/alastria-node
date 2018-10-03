5b99983f560a63f36863dc55bead6f05c0c029b5
gbarogo <36666067+gbarogo@users.noreply.github.com>
2018-10-03 13:47:22 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -105,9 +105,8 @@ else
 fi
 
 if ( [ ! -e /etc/cron.d/restart-node-cron ]); then
-    echo -e "*/3 * * * * root ~/alastria-node/scripts/restart_crontab.sh \n"  > /etc/cron.d/restart-node-cron
+    echo -e "*/3 * * * * root cd ~/alastria-node/scripts/;sudo ./restart.sh auto \n"  > /etc/cron.d/restart-node-cron
 	chmod 0644 /etc/cron.d/restart-node-cron
-	
 	/etc/init.d/cron start
 fi
 
