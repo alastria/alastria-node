093915dc591e64746c8e2e8a0ea0688e28eface4
gbarogo <36666067+gbarogo@users.noreply.github.com>
2018-10-10 12:16:00 +0200

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+function superuser {
+  if ( type "sudo"  > /dev/null 2>&1 )
+  then
+    sudo $@
+  else
+    eval $@
+  fi
+}
+
 CONSTELLATION=${ENABLE_CONSTELLATION:-}
 
 mapfile -t NODE_TYPE <~/alastria/data/NODE_TYPE
 
-if ([ -e /etc/cron.d/restart-node-cron ]); then
-    rm /etc/cron.d/restart-node-cron
+if [ -e /etc/cron.d/restart-node-cron ] && [ "reinicio" != "$1" ]; then
+    superuser rm /etc/cron.d/restart-node-cron
 fi
 
 if [ "$NODE_TYPE" == "general" ] && [ ! -z "$CONSTELLATION" ]; then
