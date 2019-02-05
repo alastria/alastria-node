524e24266dea26211408b2c69366b44548c777f3
go2chain <go2chain@babel.es>
2019-02-05 16:33:51 +0100

    Update dockerfile validator node

--- scripts/updatePerm.sh
+++ scripts/updatePerm.sh
@@ -4,23 +4,32 @@ set -e
 TMPFILE="/tmp/$(basename $0).$$.tmp"
 tmpfile=$(mktemp /tmp/updatePerm.XXXXXX)
 NODE_TYPE="$1"
+echo "El node Type es $NODE_TYPE"
 DESTDIR="$HOME/alastria/data/"
 DATADIR="$HOME/alastria-node/data/"
 
 echo "[" > $TMPFILE
 
 if [ "$NODE_TYPE" == "bootnode" ]; then
-   cat $DATADIR/boot-nodes.json $DATADIR/validator-nodes.json $DATADIR/regular-nodes.json >> $TMPFILE
+    echo "El node Type es bootnode"
+    cat $DATADIR/boot-nodes.json $DATADIR/validator-nodes.json $DATADIR/regular-nodes.json >> $TMPFILE
  else
    if [ "$NODE_TYPE" == "validator" ]; then
+    echo "El node Type es validator"
      cat $DATADIR/boot-nodes.json $DATADIR/validator-nodes.json >> $TMPFILE
  else
    if [ "$NODE_TYPE" == "general" ]; then
+    echo "El node Type es general"
      cat $DATADIR/boot-nodes.json >> $TMPFILE
   fi
   fi
 fi
 cat $TMPFILE | sed '$s/,$//' > $tmpfile
+echo "EL TMPFILE ES:"
+echo "$TMPFILE"
+echo "EL tmpfile ES:"
+echo "$tmpfile"
+
 echo "]" >> $tmpfile
 #cat $tmpfile
 cat $tmpfile > $DESTDIR/static-nodes.json
