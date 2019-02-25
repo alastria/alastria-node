355e49bb88216f654cf6e81d020897ee14a68c23
Victor Reiner <v.munozre@alumnos.urjc.es>
2019-02-25 14:23:41 +0100

    General docker modify

--- docker/node/init.sh
+++ docker/node/init.sh
@@ -1,49 +1,98 @@
 #!/bin/bash
 
-NODE_NAME,COMPANY_NAME,CPU,RAM,SEQ
-NODE_TYPE="general"
+function setnodetype {
+  PS3="Choose the node type: 1)general, 2)validator, 3)bootnode => "
+  options=("general" "validator" "bootnode")
 
+  select opt in "${options[@]}"
+  do
+    case $opt in
+      "general")
+        NODE_TYPE="general";
+        NODE_NAME="REG_";
+        break;;
+      "validator")
+        NODE_TYPE="validator";
+        NODE_NAME="VAL_";
+        break;;
+      "bootnode")
+        NODE_TYPE="bootnode";
+        NODE_NAME="BOT_";
+        break;;
+      *)
+        echo "Invalid choice '${REPLY}', please pick one of the above"
+    esac
+  done
+}
 function setcompanyname {
   echo "Write company name: "
   read COMPANY_NAME
-  echo "Company name: "$COMPANY_NAME
   echo ""
 }
 function setcpunumber {
   echo "Number of CPUs: "
   read CPU
-  echo "CPUs: "$CPU
   echo ""
 }
 function setramnumber {
   echo "Number of RAM: "
   read RAM
-  echo "RAM: "$RAM
   echo ""
 }
 function setsequential {
   echo "Sequential starting at 00: "
   read SEQ
-  echo "SEQ: "$SEQ
   echo ""
 }
-setcompanyname
-setcpunumber
-setramnumber
-setsequential
 
-NODE_NAME=$(printf "%s%s%s%s%s%s%s%s" "REG_" "$COMPANY_NAME" "_Telsius_" "$CPU" "_" "$RAM" "_" "$SEQ")
-function checkname {
-  PS3="Are you sure that these data are correct $NODE_NAME :"
+function setMonitor {
+  PS3="Do you want to install the monitor?"$'\n'"Press 1 (Yes) or 2 (No) => "
   options=("Yes" "No")
 
   select opt in "${options[@]}"
   do
     case $opt in
       "Yes")
-        echo "Starting regular node"
-        docker build -t alastria-node .
-        docker run alastria-node $NODE_TYPE $NODE_NAME
+        MONITOR=1
+        break
+        ;;
+      "No")
+        MONITOR=0
+        break
+        ;;
+    esac
+  done
+}
+
+
+setnodetype
+setcompanyname
+setcpunumber
+setramnumber
+setsequential
+setMonitor
+
+function launchnodetype {
+  if [ "validator" == "$NODE_TYPE" ]; then
+    docker run alastria-node-validator $NODE_TYPE $NODE_NAME -p 21000:21000 -p 21000:21000/udp -p 8443:8443
+  elif [ "general" == "$NODE_TYPE" ]; then
+    docker run -v "$(pwd)"/alastria:/root/alastria -p 22000:22000 -p 21000:21000 -p 21000:21000/udp -p 9000:9000 -e MONITOR_ENABLED=$MONITOR alastria-node-general --restart unless-stopped $NODE_TYPE $NODE_NAME
+  elif [ "bootnode" == "$NODE_TYPE" ]; then
+    docker run alastria-node-bootnode $NODE_TYPE $NODE_NAME -p 21000:21000 -p 21000:21000/udp
+  fi
+}
+
+function checkname {
+  PS3="Are you sure that these data are correct?"$'\n'"Node Type => $NODE_TYPE"$'\n'"Node Name => $NODE_NAME"$'\n'"Press 1 (Yes) or 2 (No) => "
+  options=("Yes" "No")
+
+  select opt in "${options[@]}"
+  do
+    case $opt in
+      "Yes")
+        echo "Starting node"
+        launchnodetype
+        # docker run alastria-node $NODE_TYPE $NODE_NAME -p 22000:22000
         ;;
       "No")
         echo "Please launch the script again"
@@ -53,4 +102,5 @@ function checkname {
   done
 }
 
+NODE_NAME=$(printf "%s%s%s%s%s%s%s%s" "$NODE_NAME" "$COMPANY_NAME" "_Telsius_" "$CPU" "_" "$RAM" "_" "$SEQ")
 checkname
