8d7f90397488c72cf93e4db360099525217fc32e
andresalo <andresalgr@gmail.com>
2019-03-06 16:33:50 +0100

    Create allow-ip.sh

---
+++ docker/scripts/allow-ip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Adds IP address to nginx whitelist
+
+if [ -z "$1" ]; then
+        echo "$0: insufficient arguments"
+        echo "Usage: ./allow-ip.sh PROXY [ -r ] IP_ADDRESS_0 [ IP_ADDRESS_1 [ IP_ADDRESS_N ] ]"
+        exit
+fi
+
+if [ $1 = "nginx" ]; then
+        DOCKER_ID=$(docker ps -aqf "name=Telsius")
+        for IP in "${@:2}"
+        do
+                docker exec $DOCKER_ID bash -c "echo \"allow $IP;\" >> /etc/nginx/whitelist"
+        done
+        docker exec "$DOCKER_ID" nginx -s reload
+fi
