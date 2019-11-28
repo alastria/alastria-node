7f925920f6ca5cde976619a8751bf9c3f7b19712
Javier Aguirre <contacto@javaguirre.net>
2019-11-28 22:12:15 +0100

    Add extra arguments to docker commands
    I want to run in detached state so the Ansible automation finishes when automatically deploys a node

--- docker/general/init.sh
+++ docker/general/init.sh
@@ -109,11 +109,27 @@ function launchNode {
   echo $DATA_DIR > $DIRECTORY/DATA_DIR
   echo $ACCESS_POINT_DIR > $DIRECTORY/ACCESS_POINT_DIR
   echo $ENABLE_CONSTELLATION > $DIRECTORY/ENABLE_CONSTELLATION
+  EXTRA_DOCKER_ARGUMENTS=${EXTRA_DOCKER_ARGUMENTS:-}
 
   if [ $MONITOR_ENABLED -eq 1 ]; then
-    docker run --name $NODE_NAME -v $DATA_DIR:/root/alastria -v $ACCESS_POINT_DIR:/etc/nginx/conf.d -p 21000:21000 -p 21000:21000/udp -p 9000:9000 -p 8443:8443 -p 80:80 -p 443:443 -e NODE_TYPE=$NODE_TYPE -e NODE_NAME=$NODE_NAME -e MONITOR_ENABLED=$MONITOR_ENABLED -e ENABLE_CONSTELLATION=$ENABLE_CONSTELLATION --restart unless-stopped alastria/alastria-node-general
+    docker run --name $NODE_NAME -v $DATA_DIR:/root/alastria \
+      ${EXTRA_DOCKER_ARGUMENTS} \
+      -v $ACCESS_POINT_DIR:/etc/nginx/conf.d \
+      -p 21000:21000 -p 21000:21000/udp -p 9000:9000 -p 8443:8443 \
+      -p 80:80 -p 443:443 \
+      -e NODE_TYPE=$NODE_TYPE -e NODE_NAME=$NODE_NAME \
+      -e MONITOR_ENABLED=$MONITOR_ENABLED \
+      -e ENABLE_CONSTELLATION=$ENABLE_CONSTELLATION \
+      --restart unless-stopped alastria/alastria-node-general
   else
-    docker run --name $NODE_NAME -v $DATA_DIR:/root/alastria -v $ACCESS_POINT_DIR:/etc/nginx/conf.d -p 21000:21000 -p 21000:21000/udp -p 9000:9000 -p 80:80 -p 443:443 -e NODE_TYPE=$NODE_TYPE -e NODE_NAME=$NODE_NAME -e MONITOR_ENABLED=$MONITOR_ENABLED -e ENABLE_CONSTELLATION=$ENABLE_CONSTELLATION --restart unless-stopped alastria/alastria-node-general
+    docker run --name $NODE_NAME -v $DATA_DIR:/root/alastria \
+      ${EXTRA_DOCKER_ARGUMENTS} \
+      -v $ACCESS_POINT_DIR:/etc/nginx/conf.d \
+      -p 21000:21000 -p 21000:21000/udp -p 9000:9000 \
+      -p 80:80 -p 443:443 \
+      -e NODE_TYPE=$NODE_TYPE -e NODE_NAME=$NODE_NAME
+      -e MONITOR_ENABLED=$MONITOR_ENABLED -e ENABLE_CONSTELLATION=$ENABLE_CONSTELLATION \
+      --restart unless-stopped alastria/alastria-node-general
   fi
 }
 
